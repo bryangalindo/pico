e638f4c3da05131747bd08369126df6b6f822cb3
Bryan Galindo <galindo.bryan08@gmail.com>
2025-05-23 17:38:37 -0400

    remove hafl-baked bounce animation

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -38,12 +38,6 @@ const HeroSection: React.FC = () => {
             See demo
           </a>
         </div>
-
-        <div className="absolute bottom-12 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center pt-2">
-            <div className="w-1 h-2 bg-white/50 rounded-full"></div>
-          </div>
-        </div>
       </div>
     </section>
   );
