bc57cb0978efc1025c817689e43fcc3419d1840d
Bryan Galindo <galindo.bryan08@gmail.com>
2025-05-24 13:15:24 -0400

    fix cors bug in prod

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
 import config as cfg
 
 app = Flask(__name__)
+CORS(app, resources={
+    r"/*": {
+        "origins": ["https://picoai.io", "http://localhost:5173"],
+        "methods": ["GET"],
+        "allow_headers": ["Content-Type", "Accept"]
+    }
+})
 
 @app.route('/ping')
 def ping():
