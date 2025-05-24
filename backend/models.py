eb8884ed2a70c456338e32141e44200f3c200105
Bryan Galindo <galindo.bryan08@gmail.com>
2025-05-24 10:56:44 -0400

    add data models

---
+++ backend/models.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Job:
+    title: str
+    url: str
+
+
+@dataclass
+class Company:
+    name: str
+    url: str
+    jobs: list[Job]
+
+
+@dataclass
+class Education:
+    name: str
+    url: str
+
+
+@dataclass
+class User:
+    first_name: str
+    last_name: str
+    url: str
+    companies: list[Company]
+    education: list[Education]
